36689f054966d9fd520fea2c7f5c7f4d0f83816d
セックス <110929010+Darykiii@users.noreply.github.com>
2024-06-11 16:18:37 -0300

    atualização foda-se

---
+++ script.js
@@ -0,0 +1,67 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-alternativas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('caixa-resultado');
+const caixaResultado = document.querySelector('.texto-resultado');
+
+const perguntas= [
+    {
+    enunciado: "Qual o prato típico de Minas Gerais?" ,
+    alternativas: [
+    "Pão de queijo.",
+    "Acarajé."
+    ]
+
+},
+
+{
+    enunciado: "Qual o prato típico do Rio Grande do Sul?" ,
+    alternativas: [
+    "carreteiro.",
+    "feijoada."
+    ]
+
+},
+{
+    enunciado: "Quais são as brincadeiras infígenas?" ,
+    alternativas: [
+"Pega-pega, amarelinha e esconde-esconde.",
+"Peteca, jogo da onça e gavião e passarinhos."
+]
+
+},
+
+{
+    enunciado: "Qual das alternativas é um prato Tupi Guarani?" ,
+    alternativas: [
+"Pirão.",
+"lasanha."
+]
+
+},
+{
+    enunciado: "Pergunta 1" ,
+    alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+]
+
+},
+
+{
+    enunciado: "Pergunta 1" ,
+    alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+]
+
+},
+
+{
+    enunciado: "Pergunta 1" ,
+    alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+]
+
+}
